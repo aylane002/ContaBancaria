da366262910b0db299590b436e8220aed1c40845
aylane002 <53566527+aylane002@users.noreply.github.com>
2020-01-20 23:21:37 -0300

    Add files via upload

---
+++ Banco/src/br/ufrn/tads/poo/Main.java
@@ -0,0 +1,29 @@
+/*package br.ufrn.tads.poo;
+
+import teste.ContaInter;
+
+public class Main {
+
+
+
+
+
+       Retangulo r = new Retangulo(2, 4);
+
+        int area = r.calcular();
+        System.out.println("Area de r:" + r.calcular());
+
+        Integer a = null;
+        Integer b = 3;
+        System.out.println(a + b);
+
+        r = new Retangulo(3, 2);
+        System.out.println("Area de r:" + r.calcular());
+    }
+
+}
+     */
+
+
+
+
